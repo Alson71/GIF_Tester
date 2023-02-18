5a5e35dbb5406bc44c0c5fc3a6c4e45a7c5399ba
Elijah Ewers <115517217+Alson71@users.noreply.github.com>
2023-02-18 00:11:27 -0500

    Update GifPlayer.java

--- src/GifPlayer.java
+++ src/GifPlayer.java
@@ -7,7 +7,54 @@ import java.time.*;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.awt.*;
+
 public class GifPlayer {
+
+    static GifPlayer gif= new GifPlayer();
+    static ImageIcon icon,icon1;
+    static JOptionPane pane,pane2;
+    static JDialog d,d2;
+    static Timer pause1;
+
+    
+    public static void main(String[] args) {
+        createFiles();
+        startProgram();
+    }
+
+
+    public static void randomColor(){
+        Random rand = new Random();
+        float red = rand.nextFloat();
+        float green = rand.nextFloat();
+        float blue = rand.nextFloat();
+        Color randomColor = new Color(red, green, blue);
+        pane.setBackground(randomColor);
+        pane.setForeground(randomColor);
+    }
+
+
+    public static void createFiles(){
+
+        icon = new ImageIcon(Objects.requireNonNull(GifPlayer.class.getResource("bruh.gif")).getFile());
+        icon1 = new ImageIcon(Objects.requireNonNull(GifPlayer.class.getResource("sturdy.gif")).getFile());
+
+        pane = new JOptionPane("",JOptionPane.INFORMATION_MESSAGE,JOptionPane.DEFAULT_OPTION,icon1,new Object[]{});
+        pane2=new JOptionPane( " ", JOptionPane.INFORMATION_MESSAGE,JOptionPane.DEFAULT_OPTION ,icon,new Object[]{});
+
+        Color color = new Color(23, 0, 5);
+        d = pane.createDialog(null, "Sturdy");
+        d2 = pane2.createDialog(null,"Wake up to Reality");
+
+        pane2.setBackground(color);
+        pane2.setForeground(color);
+
+        d.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+        d2.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+    }
+
+
+
     public void playSturdySong() {
         try {
             URL url = getClass().getResource("sturdy.wav");
@@ -23,6 +70,9 @@ public class GifPlayer {
             e.printStackTrace();
         }
     }
+
+
+
     public void playMadara() {
         try {
             URL url = getClass().getResource("wakeup.wav");
@@ -38,20 +88,12 @@ public class GifPlayer {
             e.printStackTrace();
         }
     }
-    public static void main(String[] args) {
-        GifPlayer gif= new GifPlayer();
-        ImageIcon icon = new ImageIcon(Objects.requireNonNull(GifPlayer.class.getResource("bruh.gif")).getFile());
-        ImageIcon icon1 = new ImageIcon(Objects.requireNonNull(GifPlayer.class.getResource("sturdy.gif")).getFile());
-        JOptionPane pane = new JOptionPane("",JOptionPane.INFORMATION_MESSAGE,JOptionPane.DEFAULT_OPTION,icon1,new Object[]{});
-        JOptionPane pane2=new JOptionPane( " ", JOptionPane.INFORMATION_MESSAGE,JOptionPane.DEFAULT_OPTION ,icon,new Object[]{});
-        Color color = new Color(23, 0, 5);
-        JDialog d = pane.createDialog(null, "Sturdy");
-        JDialog d2= pane2.createDialog(null,"Wake up to Reality");
-        pane2.setBackground(color);
-        pane2.setForeground(color);
-        d.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-        d2.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-        Timer pause1 = new Timer(45000, e -> {
+
+
+
+    public static void makePause(){
+         pause1 = new Timer(45000, e -> {
+
             d.setVisible(false);
             new Thread(() -> {
                 try {
@@ -70,47 +112,53 @@ public class GifPlayer {
                 }).start();
                 gif.playMadara();
                 d2.setVisible(true);
+
             }).start();
             JOptionPane.showOptionDialog(null,"Before you go....","Wait",JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE,null,new Object[]{},null);
 
         });
         pause1.setRepeats(false);
-        int bruh= JOptionPane.showConfirmDialog(null,"Would you like to get sturdy?","Sturdy Time",JOptionPane.YES_NO_OPTION,JOptionPane.INFORMATION_MESSAGE);
-        if(bruh==0){
-            LocalDateTime then = LocalDateTime.now();
-            pause1.start();
-            gif.playSturdySong();
-            do {
-                new Thread(() -> {
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    d.setVisible(false);
-                    Random rand = new Random();
-                    float red = rand.nextFloat();
-                    float green = rand.nextFloat();
-                    float blue = rand.nextFloat();
-                    Color randomColor = new Color(red, green, blue);
-                    pane.setBackground(randomColor);
-                    pane.setForeground(randomColor);
-                    int i= (int) (Math.random() * 1500);
-                    int j=(int) (Math.random() * 200);
-                    d.setLocation(i,j);
-                }).start();
-                Random rand = new Random();
-                float red = rand.nextFloat();
-                float green = rand.nextFloat();
-                float blue = rand.nextFloat();
-                Color randomColor = new Color(red, green, blue);
-                pane.setBackground(randomColor);
-                pane.setForeground(randomColor);
-                d.setVisible(true);
-            } while (ChronoUnit.SECONDS.between(then, LocalDateTime.now()) < 45);
+    }
+    
+    
+
+    public static void playFiles(){
+        LocalDateTime then = LocalDateTime.now();
+        pause1.start();
+        gif.playSturdySong();
+        do {
+            new Thread(() -> {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                d.setVisible(false);
+                randomColor();
+                int i= (int) (Math.random() * 1500);
+                int j=(int) (Math.random() * 200);
+                d.setLocation(i,j);
+            }).start();
+            randomColor();
+            d.setVisible(true);
+        } while (ChronoUnit.SECONDS.between(then, LocalDateTime.now()) < 45);
+    }
+
+    
+
+    public static void startProgram(){
+
+        makePause();
+
+        int ok= JOptionPane.showConfirmDialog(null,"Would you like to get sturdy?","Sturdy Time",JOptionPane.YES_NO_OPTION,JOptionPane.INFORMATION_MESSAGE);
+
+        if(ok==0){
+           playFiles();
         }
         else{
             System.exit(0);
         }
     }
+
+
 }
